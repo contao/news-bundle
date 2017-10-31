fb5b802b2eb3d6188fc68cc1a8d20fed2d09cb83
Leo Feyer <github@contao.org>
2017-10-31 16:12:41 +0100

    Also handle the $objNewsArchive === null case

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -626,16 +626,18 @@ class tl_news extends Backend
 		{
 			$autoAlias = true;
 			$slugOptions = [];
-			$objNewsArchive = NewsArchiveModel::findByPk($dc->activeRecord->pid);
-			$objPage = PageModel::findWithDetails($objNewsArchive->jumpTo);
-			if ($objPage)
+
+			// Read the slug options from the associated page
+			if (($objNewsArchive = NewsArchiveModel::findByPk($dc->activeRecord->pid)) !== null && ($objPage = PageModel::findWithDetails($objNewsArchive->jumpTo)) !== null)
 			{
 				$slugOptions['locale'] = $objPage->language;
+
 				if ($objPage->validAliasCharacters)
 				{
 					$slugOptions['validChars'] = $objPage->validAliasCharacters;
 				}
 			}
+
 			$varValue = System::getContainer()->get('contao.slug.generator')->generate(StringUtil::stripInsertTags($dc->activeRecord->headline), $slugOptions);
 		}
 
